b654fe5e8a1e858bc2f182b804a4ba167b0f3090
Allero CI <ci@allero.io>
2022-09-18 08:24:23 +0000

    Brew formula update for allero version 0.0.7

--- allero.rb
+++ allero.rb
@@ -3,22 +3,22 @@
 class Allero < Formula
   desc ""
   homepage "https://allero.io/"
-  version "0.0.6"
+  version "0.0.7"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/allero-io/allero/releases/download/0.0.6/allero_0.0.6_Darwin_x86_64.zip"
-    sha256 "3632c8ded407be148a5d6942d1e49c5038f52233c9c7c3ef2514c33572d7cad4"
+    url "https://github.com/allero-io/allero/releases/download/0.0.7/allero_0.0.7_Darwin_x86_64.zip"
+    sha256 "bd3c1aa2222f39ea813e7dd0f81da5debf73d000aab00bb55ac41b725bab35e3"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/allero-io/allero/releases/download/0.0.6/allero_0.0.6_Darwin_arm64.zip"
-    sha256 "11ab8ea978ca733ae3da3226dc16b181c78d6245d0c198318e72fea3d9f533d5"
+    url "https://github.com/allero-io/allero/releases/download/0.0.7/allero_0.0.7_Darwin_arm64.zip"
+    sha256 "9c466e6b1293dbd76813430d1396597773859168edd7ab5f81a0488c8c268baa"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/allero-io/allero/releases/download/0.0.6/allero_0.0.6_Linux_x86_64.zip"
-    sha256 "c489e17692abc1a3fa87409097c281e08225713d2e64024c46a7fa1f135ace43"
+    url "https://github.com/allero-io/allero/releases/download/0.0.7/allero_0.0.7_Linux_x86_64.zip"
+    sha256 "b0ffc72d4c71dd07e46bd01fe7da650c0a6efe15d150be87c51a963b746147a2"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/allero-io/allero/releases/download/0.0.6/allero_0.0.6_Linux_arm64.zip"
-    sha256 "03f5d216f5b5cbbed09bb4b5531af95c12b8368c37aaca9fa2e71df589ee65e6"
+    url "https://github.com/allero-io/allero/releases/download/0.0.7/allero_0.0.7_Linux_arm64.zip"
+    sha256 "a232a32cbb65e3846ea4b316299c6e0daadbd5d2c1cb78059207086b0f865071"
   end
   def install
     bin.install "allero"
