f11b63b43899b254212f367281c804392fc9e51b
Allero CI <ci@allero.io>
2022-09-22 08:25:08 +0000

    Brew formula update for allero version 0.0.15

--- allero.rb
+++ allero.rb
@@ -2,30 +2,30 @@
 # frozen_string_literal: true
 class Allero < Formula
   desc ""
-  homepage "https://allero.io/"
-  version "0.0.14"
+  homepage "https://www.allero.io/"
+  version "0.0.15"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/allero-io/allero/releases/download/0.0.14/allero_0.0.14_Darwin_x86_64.zip"
-    sha256 "ef7fa978e71ea49c50479a13c5af005dd0d14ff0375f6627991c51031de58d76"
+    url "https://github.com/allero-io/allero/releases/download/0.0.15/allero_0.0.15_Darwin_x86_64.zip"
+    sha256 "7dc1f2701247f6f1f12150a7535d98b9c846189c44d9fedaa25e94c72dfb6930"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/allero-io/allero/releases/download/0.0.14/allero_0.0.14_Darwin_arm64.zip"
-    sha256 "e23f5f454b2d123f6fee686aac3d97a39232eca473b966da6bb19b7e7a1002e0"
+    url "https://github.com/allero-io/allero/releases/download/0.0.15/allero_0.0.15_Darwin_arm64.zip"
+    sha256 "d56cd3057c396e8b1bed5be810bc0aab01c27f24a8c3514188188e4810496a12"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/allero-io/allero/releases/download/0.0.14/allero_0.0.14_Linux_x86_64.zip"
-    sha256 "4c1dad70841de7c499800a65b060d0c393efa5d583f62c311aca856555d9cc31"
+    url "https://github.com/allero-io/allero/releases/download/0.0.15/allero_0.0.15_Linux_x86_64.zip"
+    sha256 "af406a8f1494785964db8427835ef1aa7d8de4b994bb5c9375b15cb5781562aa"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/allero-io/allero/releases/download/0.0.14/allero_0.0.14_Linux_arm64.zip"
-    sha256 "dad7e99e2360d0b97261882b50bbbc804ceaf5dca0c72c4f433cd5cd2bad481d"
+    url "https://github.com/allero-io/allero/releases/download/0.0.15/allero_0.0.15_Linux_arm64.zip"
+    sha256 "69ff256402b4b60db2ed91542b13fc56c2cddad809146c2c88b07f752e27d43c"
   end
   def install
     bin.install "allero"
   end
   def caveats
     <<~EOS
-      \033[32m[V] Downloaded Datree
+      \033[32m[V] Downloaded Allero
       [V] Finished Installation
       \033[35m Usage: $ allero fetch github <owner|owner/repo ...>
     EOS
